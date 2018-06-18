d0988704f787e9648449a6bc73dc0dd5278ef30c
Giulio Vian <giuliovdev@hotmail.com>
2018-06-19 00:08:34 +0100

    add.rule now uploads a template function
    TODO: generate function code from rule source

--- src/aggregator-cli/Rules/AggregatorRules.cs
+++ src/aggregator-cli/Rules/AggregatorRules.cs
@@ -5,6 +5,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -80,7 +81,7 @@ namespace aggregator.cli
 
         internal async Task AddAsync(string instance, string name, string filePath)
         {
-            byte[] zipContent = CreateTemporaryZipForRule(filePath);
+            byte[] zipContent = CreateTemporaryZipForRule(name, filePath);
 
             var instances = new AggregatorInstances(azure);
             (string username, string password) = instances.GetPublishCredentials(instance);
@@ -88,19 +89,38 @@ namespace aggregator.cli
             await UploadZipWithRule(instance, zipContent, username, password);
         }
 
-        private static byte[] CreateTemporaryZipForRule(string filePath)
+        private static byte[] CreateTemporaryZipForRule(string name, string filePath)
         {
             // see https://docs.microsoft.com/en-us/azure/azure-functions/deployment-zip-push
+
+            // working directory
             var rand = new Random((int)DateTime.UtcNow.Ticks);
-            string tempDirPath = Path.Combine(
+            string baseDirPath = Path.Combine(
                 Path.GetTempPath(),
                 $"aggregator-{rand.Next().ToString()}");
+            string tempDirPath = Path.Combine(
+                baseDirPath,
+                name);
             Directory.CreateDirectory(tempDirPath);
+
+            // copy content
             File.Copy(filePath, Path.Combine(tempDirPath, Path.GetFileName(filePath)));
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream reader = assembly.GetManifestResourceStream("aggregator.cli.Rules.function.json"))
+            using (var writer = File.Create(Path.Combine(tempDirPath, "function.json")))
+            {
+                reader.CopyTo(writer);
+            }
+            using (Stream reader = assembly.GetManifestResourceStream("aggregator.cli.Rules.run.csx"))
+            using (var writer = File.Create(Path.Combine(tempDirPath, "run.csx")))
+            {
+                reader.CopyTo(writer);
+            }
+
+            // zip
             string tempZipPath = Path.GetTempFileName();
             File.Delete(tempZipPath);
-
-            ZipFile.CreateFromDirectory(tempDirPath, tempZipPath);
+            ZipFile.CreateFromDirectory(baseDirPath, tempZipPath);
             var zipContent = File.ReadAllBytes(tempZipPath);
 
             // clean-up: everything is in memory
@@ -113,6 +133,8 @@ namespace aggregator.cli
         {
             var client = new HttpClient();
             client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("aggregator", "3.0"));
+            // POST /api/zipdeploy?isAsync=true
+            // Deploy from zip asynchronously. The Location header of the response will contain a link to a pollable deployment status.
             string apiUrl = $"https://{instance}.scm.azurewebsites.net/api/zipdeploy";
             string base64AuthInfo = Convert.ToBase64String(Encoding.ASCII.GetBytes(($"{username}:{password}")));
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64AuthInfo);
