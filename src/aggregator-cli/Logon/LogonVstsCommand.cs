1e79736f224e2cc23c9374bb4ae75c2a9ee57d07
Giulio Vian <giuliovdev@hotmail.com>
2018-06-19 19:47:26 +0100

    draft Vsts logon

--- src/aggregator-cli/Logon/LogonVstsCommand.cs
+++ src/aggregator-cli/Logon/LogonVstsCommand.cs
@@ -10,14 +10,17 @@ namespace aggregator.cli
     {
         // HACK: CommandLineParser is case sensitive, sigh!
         integrated = 0,
-        Integrated = 0,
+        Integrated = integrated,
         pat = 1,
-        PAT = 1,
+        PAT = pat,
     }
 
     [Verb("logon.vsts", HelpText = "Logon into Visual Studio Team Services.")]
     class LogonVstsCommand : CommandBase
     {
+        [Option('u', "url", Required = true, HelpText = "Account/server URL, e.g. myaccount.visualstudio.com .")]
+        public string Url { get; set; }
+
         [Option('m', "mode", Required = true, HelpText = "Logon mode.")]
         public VstsLogonMode Mode { get; set; }
 
@@ -26,7 +29,21 @@ namespace aggregator.cli
 
         internal override Task<int> RunAsync()
         {
-            return Task.Run(() => 2);
+            var data = new VstsLogon()
+            {
+                Url = this.Url,
+                Mode = this.Mode,
+                Token = this.Token
+            };
+            string path = data.Save();
+            // now check for validity
+            var vsts = data.Logon();
+            if (vsts == null)
+            {
+                WriteError("Invalid VSTS credentials");
+                return Task.Run(() => 2);
+            }
+            return Task.Run(() => 0);
         }
     }
 }
---
+++ src/aggregator-cli/Logon/VstsLogon.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aggregator.cli
+{
+    class VstsLogon
+    {
+        private static readonly string LogonDataTag = "davs";
+
+        public string Url { get; set; }
+        public VstsLogonMode Mode { get; set; }
+        public string Token { get; set; }
+
+        public string Save()
+        {
+            return new LogonDataStore(LogonDataTag).Save(this);
+        }
+
+        public static VstsLogon Load()
+        {
+            return new LogonDataStore(LogonDataTag).Load<VstsLogon>();
+        }
+
+        public async Task<VssConnection> Logon()
+        {
+            var clientCredentials = default(VssCredentials);
+            switch (Mode)
+            {
+                case VstsLogonMode.Integrated:
+                    clientCredentials = new VssCredentials();
+                    break;
+                case VstsLogonMode.PAT:
+                    clientCredentials=new VssBasicCredential("pat", Token);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(Mode));
+            }
+            var connection = new VssConnection(new Uri(Url), clientCredentials);
+            await connection.ConnectAsync();
+            return connection;
+        }
+    }
+}
