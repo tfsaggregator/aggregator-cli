ef3df61b913d443f02aded9c72c0948a996ccd74
Giulio Vian <giuliovdev@hotmail.com>
2018-06-19 19:46:56 +0100

    Refactor secret data store to be shared by multiple logon type

--- src/aggregator-cli/Logon/AzureLogon.cs
+++ src/aggregator-cli/Logon/AzureLogon.cs
@@ -12,69 +12,22 @@ namespace aggregator.cli
 {
     class AzureLogon
     {
-        private static readonly byte[] magic = new byte[] { 0x44, 0x41, 0x41, 0x5A };
+        private static readonly string LogonDataTag = "daaz";
 
         public string SubscriptionId { get; set; }
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
         public string TenantId { get; set; }
-        protected static byte[] Magic { get => magic; }
 
-        protected static string LogonAzureDataPath
-        {
-            get
-            {
-                return Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                    "aggregator-cli",
-                    "daaz.dat");
-            }
-        }
 
         public string Save()
         {
-            string logonAzureDataString = JsonConvert.SerializeObject(this);
-            var logonAzureData = UnicodeEncoding.UTF8.GetBytes(logonAzureDataString);
-
-            var entropy = new byte[16];
-            new RNGCryptoServiceProvider().GetBytes(entropy);
-            var encryptedData = ProtectedData.Protect(logonAzureData, entropy, DataProtectionScope.CurrentUser);
-
-            string logonAzureDataPath = LogonAzureDataPath;
-            // make sure exists
-            Directory.CreateDirectory(Path.GetDirectoryName(logonAzureDataPath));
-
-            using (var stream = new FileStream(logonAzureDataPath, FileMode.OpenOrCreate))
-            {
-                stream.Write(Magic, 0, Magic.Length);
-                stream.Write(entropy, 0, entropy.Length);
-                stream.Write(encryptedData, 0, encryptedData.Length);
-            }
-
-            return logonAzureDataPath;
+            return new LogonDataStore(LogonDataTag).Save(this);
         }
 
         public static AzureLogon Load()
         {
-            if (!File.Exists(LogonAzureDataPath))
-                return null;
-
-            var entropy = new byte[16];
-            byte[] outBuffer;
-            using (var stream = new FileStream(LogonAzureDataPath, FileMode.Open))
-            {
-                var magicBuffer = new byte[Magic.Length];
-                stream.Read(magicBuffer, 0, magicBuffer.Length);
-                if (magicBuffer == Magic)
-                    throw new InvalidDataException("Invalud credential file");
-                var inBuffer = new byte[stream.Length];
-                stream.Read(entropy, 0, entropy.Length);
-                stream.Read(inBuffer, 0, inBuffer.Length);
-                outBuffer = ProtectedData.Unprotect(inBuffer, entropy, DataProtectionScope.CurrentUser);
-            }
-            var logonAzureDataString = UnicodeEncoding.UTF8.GetString(outBuffer);
-            var result = JsonConvert.DeserializeObject<AzureLogon>(logonAzureDataString);
-            return result;
+            return new LogonDataStore(LogonDataTag).Load<AzureLogon>();
         }
 
         public IAzure Logon()
---
+++ src/aggregator-cli/Logon/LogonDataStore.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace aggregator.cli
+{
+    class LogonDataStore
+    {
+        public LogonDataStore(string tag) => this.Tag = tag;
+
+        public string Tag { get; private set; }
+        public byte[] Magic => Encoding.ASCII.GetBytes(Tag);
+        protected string LogonDataPath
+        {
+            get
+            {
+                return Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    "aggregator-cli",
+                    Tag + ".dat");
+            }
+        }
+
+        public string Save<T>(T data)
+        {
+            string logonDataString = JsonConvert.SerializeObject(data);
+            var logonData = UnicodeEncoding.UTF8.GetBytes(logonDataString);
+
+            var entropy = new byte[16];
+            new RNGCryptoServiceProvider().GetBytes(entropy);
+            var encryptedData = ProtectedData.Protect(logonData, entropy, DataProtectionScope.CurrentUser);
+
+            string logonADataPath = LogonDataPath;
+            // make sure exists
+            Directory.CreateDirectory(Path.GetDirectoryName(logonADataPath));
+
+            using (var stream = new FileStream(logonADataPath, FileMode.OpenOrCreate))
+            {
+                stream.Write(Magic, 0, Magic.Length);
+                stream.Write(entropy, 0, entropy.Length);
+                stream.Write(encryptedData, 0, encryptedData.Length);
+            }
+
+            return logonADataPath;
+        }
+
+        public T Load<T>()
+        {
+            if (!File.Exists(LogonDataPath))
+                return default(T);
+
+            var entropy = new byte[16];
+            byte[] outBuffer;
+            using (var stream = new FileStream(LogonDataPath, FileMode.Open))
+            {
+                var magicBuffer = new byte[Magic.Length];
+                stream.Read(magicBuffer, 0, magicBuffer.Length);
+                if (magicBuffer == Magic)
+                    throw new InvalidDataException("Invalid credential file");
+                var inBuffer = new byte[stream.Length];
+                stream.Read(entropy, 0, entropy.Length);
+                stream.Read(inBuffer, 0, inBuffer.Length);
+                outBuffer = ProtectedData.Unprotect(inBuffer, entropy, DataProtectionScope.CurrentUser);
+            }
+            var logonAzureDataString = UnicodeEncoding.UTF8.GetString(outBuffer);
+            var result = JsonConvert.DeserializeObject<T>(logonAzureDataString);
+            return result;
+        }
+    }
+}
