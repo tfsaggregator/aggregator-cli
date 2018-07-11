f56011662eb1913dcf3385c4cdca9c9c16604b3d
Giulio Vian <giuliovdev@hotmail.com>
2018-07-11 23:09:50 +0100

    Hello message

--- src/aggregator-cli/CommandBase.cs
+++ src/aggregator-cli/CommandBase.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.Services.WebApi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace aggregator.cli
@@ -28,10 +29,19 @@ namespace aggregator.cli
         {
             try
             {
+                var logger = new ConsoleLogger();
+                var title = GetCustomAttribute<AssemblyTitleAttribute>();
+                var config = GetCustomAttribute<AssemblyConfigurationAttribute>();
+                var fileVersion = GetCustomAttribute<AssemblyFileVersionAttribute>();
+                var infoVersion = GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+                var copyright = GetCustomAttribute<AssemblyCopyrightAttribute>();
+
+                // Hello World
+                logger.WriteInfo($"{title.Title} v{infoVersion.InformationalVersion} (build: {fileVersion.Version} {config.Configuration}) (c) {copyright.Copyright}");
+
                 var t = cmd.RunAsync();
                 t.Wait();
                 int rc = t.Result;
-                var logger = new ConsoleLogger();
                 if (rc != 0)
                 {
                     logger.WriteError("Failed!");
@@ -47,6 +57,12 @@ namespace aggregator.cli
                 logger.WriteError(ex.Message);
                 return 99;
             }
+        }
+
+        private static T GetCustomAttribute<T>()
+            where T : Attribute
+        {
+            return Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(T), false).FirstOrDefault() as T;
         }
     }
 }
