db5132d21749b39f0886b438c6d822e66be0225b
BobSilent <andre_1@gmx.net>
2020-01-30 22:02:42 +0100

    change function run.csx signature from HttpResponseMessage,HttpRequestMessage to IActionResult, HttpRequest solves #104, #105 within Function V3

--- src/aggregator-cli/Rules/run.csx
+++ src/aggregator-cli/Rules/run.csx
@@ -1,13 +1,17 @@
 ﻿#r "../bin/aggregator-function.dll"
 #r "../bin/aggregator-shared.dll"
+#r "../bin/Microsoft.VisualStudio.Services.ServiceHooks.WebApi.dll"
 
 using System.Threading;
-
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
 using aggregator;
 
-public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, ILogger logger, Microsoft.Azure.WebJobs.ExecutionContext context, CancellationToken cancellationToken)
+public static async Task<IActionResult> Run(HttpRequest req, ILogger logger, ExecutionContext executionContext, CancellationToken cancellationToken)
 {
-    var handler = new AzureFunctionHandler(logger, context);
-    var result = await handler.RunAsync(req, cancellationToken);
+    var handler = new AzureFunctionHandler(logger, executionContext, req.HttpContext);
+    var eventData = await req.GetWebHookEvent(logger);
+    var result = await handler.RunAsync(eventData, cancellationToken);
     return result;
 }
--- src/aggregator-function/AzureFunctionHandler.cs
+++ src/aggregator-function/AzureFunctionHandler.cs
@@ -1,18 +1,20 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using aggregator.Engine;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
 using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.ServiceHooks.WebApi;
 using Microsoft.VisualStudio.Services.WebApi;
-
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
 
@@ -24,49 +26,51 @@ namespace aggregator
     public class AzureFunctionHandler
     {
         private readonly ILogger _log;
-        private readonly ExecutionContext _context;
+        private readonly ExecutionContext _executionContext;
 
-        public AzureFunctionHandler(ILogger logger, ExecutionContext context)
+
+        public AzureFunctionHandler(ILogger logger, ExecutionContext executionContext, HttpContext httpContext)
         {
             _log = logger;
-            _context = context;
+            _executionContext = executionContext;
+            HttpContext = httpContext;
         }
 
-        public async Task<HttpResponseMessage> RunAsync(HttpRequestMessage req, CancellationToken cancellationToken)
+        public async Task<IActionResult> RunAsync(/*HttpRequest request, */WebHookEvent eventData, CancellationToken cancellationToken)
         {
-            _log.LogDebug($"Context: {_context.InvocationId} {_context.FunctionName} {_context.FunctionDirectory} {_context.FunctionAppDirectory}");
+            _log.LogDebug($"Context: {_executionContext.InvocationId} {_executionContext.FunctionName} {_executionContext.FunctionDirectory} {_executionContext.FunctionAppDirectory}");
 
-            var ruleName = _context.FunctionName;
+            var ruleName = _executionContext.FunctionName;
             var aggregatorVersion = GetCustomAttribute<System.Reflection.AssemblyInformationalVersionAttribute>()?.InformationalVersion;
             _log.LogInformation($"Aggregator v{aggregatorVersion} executing rule '{ruleName}'");
             cancellationToken.ThrowIfCancellationRequested();
 
-            // Get request body
-            var eventData = await GetWebHookEvent(req);
             if (eventData == null)
             {
-                return req.CreateErrorResponse(HttpStatusCode.BadRequest, "Request body is empty");
+                return BadRequest("Request body is empty");
             }
 
             // sanity check
             if (!DevOpsEvents.IsValidEvent(eventData.EventType)
                 || eventData.PublisherId != DevOpsEvents.PublisherId)
             {
-                return req.CreateErrorResponse(HttpStatusCode.BadRequest, "Not a good Azure DevOps post...");
+                _log.LogDebug("return BadRequest");
+                return BadRequest(new {Error = "Not a good Azure DevOps post..."});
             }
 
             var eventContext = CreateContextFromEvent(eventData);
             if (eventContext.IsTestEvent())
             {
-                return req.CreateTestEventResponse(aggregatorVersion, ruleName);
+                Response.AddCustomHeaders(aggregatorVersion, ruleName);
+                return RespondToTestEventMessage(aggregatorVersion, ruleName);
             }
 
             var configContext = GetConfigurationContext();
             var configuration = AggregatorConfiguration.ReadConfiguration(configContext)
-                                                       .UpdateFromUrl(ruleName, req.RequestUri);
+                                                       .UpdateFromUrl(ruleName, GetRequestUri());
 
             var logger = new ForwarderLogger(_log);
-            var ruleProvider = new AzureFunctionRuleProvider(logger, _context.FunctionDirectory);
+            var ruleProvider = new AzureFunctionRuleProvider(logger, _executionContext.FunctionDirectory);
             var ruleExecutor = new RuleExecutor(logger, configuration);
             using (_log.BeginScope($"WorkItem #{eventContext.WorkItemPayload.WorkItem.Id}"))
             {
@@ -77,45 +81,44 @@ namespace aggregator
 
                     if (string.IsNullOrEmpty(execResult))
                     {
-                        return req.CreateResponse(HttpStatusCode.OK);
+                        return Ok();
                     }
                     else
                     {
                         _log.LogInformation($"Returning '{execResult}' from '{rule.Name}'");
-                        return req.CreateResponse(HttpStatusCode.OK, execResult);
+                        return Ok(execResult);
                     }
                 }
                 catch (Exception ex)
                 {
                     _log.LogWarning($"Rule '{ruleName}' failed: {ex.Message}");
-                    return req.CreateErrorResponse(HttpStatusCode.NotImplemented, ex);
+                    return BadRequest(ex.Message);
                 }
             }
         }
 
 
-        private IConfigurationRoot GetConfigurationContext()
+        private IConfiguration GetConfigurationContext()
         {
             var config = new ConfigurationBuilder()
-                         .SetBasePath(_context.FunctionAppDirectory)
+                         .SetBasePath(_executionContext.FunctionAppDirectory)
                          .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
                          .AddEnvironmentVariables()
                          .Build();
             return config;
         }
 
-
-        private async Task<WebHookEvent> GetWebHookEvent(HttpRequestMessage req)
+        private Uri GetRequestUri()
         {
-            var jsonContent = await req.Content.ReadAsStringAsync();
-            if (string.IsNullOrWhiteSpace(jsonContent))
-            {
-                _log.LogWarning($"Failed parsing request body: empty");
-                return null;
-            }
+            //during testing this can be null
+            var requestUrl = Request?.GetDisplayUrl() ?? "https://google.com/";
+            return new Uri(requestUrl);
+        }
 
-            var data = JsonConvert.DeserializeObject<WebHookEvent>(jsonContent);
-            return data;
+
+        private IActionResult RespondToTestEventMessage(string aggregatorVersion, string ruleName)
+        {
+            return Ok(new { message = $"Hello from Aggregator v{aggregatorVersion} executing rule '{ruleName}'" });
         }
 
         protected WorkItemEventContext CreateContextFromEvent(WebHookEvent eventData)
@@ -200,24 +203,106 @@ namespace aggregator
             where T : Attribute
         {
             return System.Reflection.Assembly
-                .GetExecutingAssembly()
-                .GetCustomAttributes(typeof(T), false)
-                .FirstOrDefault() as T;
+                         .GetExecutingAssembly()
+                         .GetCustomAttributes(typeof(T), false)
+                         .FirstOrDefault() as T;
         }
+
+
+        #region Microsoft.AspNetCore.Mvc.ControllerBase
+
+        /// <summary>
+        /// Gets the <see cref="Http.HttpContext"/> for the executing action.
+        /// </summary>
+        public HttpContext HttpContext { get; }
+
+        /// <summary>
+        /// Gets the <see cref="HttpRequest"/> for the executing action.
+        /// </summary>
+        public HttpRequest Request => HttpContext?.Request;
+
+        /// <summary>
+        /// Gets the <see cref="HttpResponse"/> for the executing action.
+        /// </summary>
+        public HttpResponse Response => HttpContext?.Response;
+
+        /// <summary>
+        /// Creates an <see cref="BadRequestResult"/> that produces a <see cref="StatusCodes.Status400BadRequest"/> response.
+        /// </summary>
+        /// <returns>The created <see cref="BadRequestResult"/> for the response.</returns>
+        [NonAction]
+        public virtual BadRequestResult BadRequest()
+            => new BadRequestResult();
+
+        /// <summary>
+        /// Creates an <see cref="BadRequestObjectResult"/> that produces a <see cref="StatusCodes.Status400BadRequest"/> response.
+        /// </summary>
+        /// <param name="error">An error object to be returned to the client.</param>
+        /// <returns>The created <see cref="BadRequestObjectResult"/> for the response.</returns>
+        [NonAction]
+        public virtual BadRequestObjectResult BadRequest(object error)
+            => new BadRequestObjectResult(error);
+
+
+        /// <summary>
+        /// Creates a <see cref="OkResult"/> object that produces an empty <see cref="StatusCodes.Status200OK"/> response.
+        /// </summary>
+        /// <returns>The created <see cref="OkResult"/> for the response.</returns>
+        [NonAction]
+        public virtual OkResult Ok()
+            => new OkResult();
+
+        /// <summary>
+        /// Creates an <see cref="OkObjectResult"/> object that produces an <see cref="StatusCodes.Status200OK"/> response.
+        /// </summary>
+        /// <param name="value">The content value to format in the entity body.</param>
+        /// <returns>The created <see cref="OkObjectResult"/> for the response.</returns>
+        [NonAction]
+        public virtual OkObjectResult Ok(object value)
+            => new OkObjectResult(value);
+
+        /// <summary>
+        /// Creates an <see cref="NotFoundResult"/> that produces a <see cref="StatusCodes.Status404NotFound"/> response.
+        /// </summary>
+        /// <returns>The created <see cref="NotFoundResult"/> for the response.</returns>
+        [NonAction]
+        public virtual NotFoundResult NotFound()
+            => new NotFoundResult();
+
+        /// <summary>
+        /// Creates an <see cref="NotFoundObjectResult"/> that produces a <see cref="StatusCodes.Status404NotFound"/> response.
+        /// </summary>
+        /// <returns>The created <see cref="NotFoundObjectResult"/> for the response.</returns>
+        [NonAction]
+        public virtual NotFoundObjectResult NotFound(object value)
+            => new NotFoundObjectResult(value);
+
+        #endregion
     }
 
 
-    internal static class HttpResponseMessageExtensions
+    public static class HttpExtensions
     {
-        public static HttpResponseMessage CreateTestEventResponse(this HttpRequestMessage req, string aggregatorVersion, string ruleName)
+        public static HttpResponse AddCustomHeaders(this HttpResponse response, string aggregatorVersion, string ruleName)
         {
-            var resp = req.CreateResponse(HttpStatusCode.OK, new
-                                                             {
-                                                                 message = $"Hello from Aggregator v{aggregatorVersion} executing rule '{ruleName}'"
-                                                             });
-            resp.Headers.Add("X-Aggregator-Version", aggregatorVersion);
-            resp.Headers.Add("X-Aggregator-Rule", ruleName);
-            return resp;
+            response?.Headers.Add("X-Aggregator-Version", aggregatorVersion);
+            response?.Headers.Add("X-Aggregator-Rule", ruleName);
+
+            return response;
+        }
+
+        public static async Task<WebHookEvent> GetWebHookEvent(this HttpRequest request, ILogger logger)
+        {
+            using var bodyStreamReader = new StreamReader(request.Body);
+            var jsonContent = await bodyStreamReader.ReadToEndAsync();
+            if (string.IsNullOrWhiteSpace(jsonContent))
+            {
+                logger.LogWarning($"Failed parsing request body: empty");
+                return null;
+            }
+
+            var data = JsonConvert.DeserializeObject<WebHookEvent>(jsonContent);
+            return data;
         }
     }
 }
