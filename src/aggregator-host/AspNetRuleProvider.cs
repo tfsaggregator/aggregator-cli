4c48cc05909b60bd4a7a94eabf702bb5662704ad
Giulio Vian <giuliovdev@hotmail.com>
2020-08-09 19:44:43 +0200

    initial working asp.net host

---
+++ src/aggregator-host/AspNetRuleProvider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using aggregator.Engine;
+using aggregator.Engine.Language;
+using Microsoft.Extensions.FileProviders;
+
+namespace aggregator
+{
+    internal class AspNetRuleProvider : IRuleProvider
+    {
+        private const string SCRIPT_RULE_DIRECTORY = "rules";
+        private const string SCRIPT_RULE_NAME_PATTERN = ".rule";
+
+        private readonly IAggregatorLogger _logger;
+        private readonly string _rulesPath;
+
+        public AspNetRuleProvider(IAggregatorLogger logger, string applicationRoot)
+        {
+            _logger = logger;
+            _rulesPath = applicationRoot;
+        }
+
+        /// <inheritdoc />
+        public async Task<IRule> GetRule(string ruleName)
+        {
+            var ruleFilePath = GetRuleFilePath(ruleName);
+            var (preprocessedRule, _) = await RuleFileParser.ReadFile(ruleFilePath);
+
+            return new ScriptedRuleWrapper(ruleName, preprocessedRule);
+        }
+
+        private string GetRuleFilePath(string ruleName)
+        {
+            bool IsRequestedRule(IFileInfo info)
+            {
+                return string.Equals(ruleName, Path.GetFileNameWithoutExtension(info.Name), StringComparison.OrdinalIgnoreCase);
+            }
+
+            var provider = new PhysicalFileProvider(_rulesPath);
+            var contents = provider.GetDirectoryContents(SCRIPT_RULE_DIRECTORY).Where(f=>f.Name.EndsWith(SCRIPT_RULE_NAME_PATTERN));
+
+            var ruleFilePath = contents.First(IsRequestedRule)?.PhysicalPath;
+
+            if (ruleFilePath == null)
+            {
+                var errorMsg = $"Rule code file '{ruleName}.rule' not found at expected Path {_rulesPath}";
+                _logger.WriteError(errorMsg);
+                throw new FileNotFoundException(errorMsg);
+            }
+
+            _logger.WriteVerbose($"Rule code found at {ruleFilePath}");
+            return ruleFilePath;
+        }
+    }
+}
---
+++ src/aggregator-host/Controllers/WorkItemController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using aggregator;
+using aggregator.Engine;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.Services.ServiceHooks.WebApi;
+
+namespace aggregator_host.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WorkItemController : ControllerBase
+    {
+        private readonly ILogger<WorkItemController> _log;
+        private readonly IConfiguration _configuration;
+
+        public WorkItemController(IConfiguration configuration, ILogger<WorkItemController> logger)
+        {
+            _configuration = configuration;
+            _log = logger;
+        }
+
+        [HttpGet]
+        public string Get(string rule)
+        {
+            _log.LogInformation("Get!");
+            return "Hi from self-hosted!";
+        }
+
+        [HttpPost("{ruleName}")]
+        public async Task<IActionResult> PostAsync(WebHookEvent eventData, string ruleName, CancellationToken cancellationToken)
+        {
+            var aggregatorVersion = RequestHelper.AggregatorVersion;
+            _log.LogInformation($"Aggregator v{aggregatorVersion} executing rule '{ruleName}'");
+
+            if (eventData == null)
+            {
+                return BadRequest("Request body is empty");
+            }
+
+            // sanity check
+            if (!DevOpsEvents.IsValidEvent(eventData.EventType)
+                || eventData.PublisherId != DevOpsEvents.PublisherId)
+            {
+                _log.LogDebug("return BadRequest");
+                return BadRequest(new { Error = "Not a good Azure DevOps post..." });
+            }
+
+            var helper = new RequestHelper(_log);
+            var eventContext = helper.CreateContextFromEvent(eventData);
+            if (eventContext.IsTestEvent())
+            {
+                Response.AddCustomHeaders(aggregatorVersion, ruleName);
+                return RespondToTestEventMessage(aggregatorVersion, ruleName);
+            }
+
+            var configContext = _configuration;
+            var configuration = AggregatorConfiguration.ReadConfiguration(configContext)
+                                                       .UpdateFromUrl(ruleName, GetRequestUri());
+
+            var logger = new ForwarderLogger(_log);
+            var ruleProvider = new AspNetRuleProvider(logger, _configuration.GetValue<string>(WebHostDefaults.ContentRootKey));
+            var ruleExecutor = new RuleExecutor(logger, configuration);
+            using (_log.BeginScope($"WorkItem #{eventContext.WorkItemPayload.WorkItem.Id}"))
+            {
+                try
+                {
+                    var rule = await ruleProvider.GetRule(ruleName);
+                    var execResult = await ruleExecutor.ExecuteAsync(rule, eventContext, cancellationToken);
+
+                    if (string.IsNullOrEmpty(execResult))
+                    {
+                        return Ok();
+                    }
+                    else
+                    {
+                        _log.LogInformation($"Returning '{execResult}' from '{rule.Name}'");
+                        return Ok(execResult);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _log.LogWarning($"Rule '{ruleName}' failed: {ex.Message}");
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+
+        private Uri GetRequestUri()
+        {
+            //during testing this can be null
+            var requestUrl = Request?.GetDisplayUrl() ?? "https://google.com/";
+            return new Uri(requestUrl);
+        }
+
+        private IActionResult RespondToTestEventMessage(string aggregatorVersion, string ruleName)
+        {
+            return Ok(new { message = RequestHelper.GetTestEventMessageReply(aggregatorVersion, ruleName) });
+        }
+    }
+}
