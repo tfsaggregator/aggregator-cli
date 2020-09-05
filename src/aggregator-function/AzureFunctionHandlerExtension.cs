a484e532b4a0bb97b1ce848dfbbf3923f0d7d6ac
Giulio Vian <giuliovdev@hotmail.com>
2020-09-05 14:44:56 +0200

    resolve Code Smells in function project

--- src/aggregator-function/AzureFunctionHandlerExtension.cs
+++ src/aggregator-function/AzureFunctionHandlerExtension.cs
@@ -9,15 +9,19 @@ namespace aggregator
         /// <summary>
         /// This method exists for backward compatibility reasons.
         /// </summary>
-        /// <param name="this"></param>
         /// <param name="req"></param>
         /// <returns></returns>
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+#pragma warning disable IDE0060 // Remove unused parameter
         public static async Task<HttpResponseMessage> Run(this AzureFunctionHandler @this, HttpRequestMessage request)
+#pragma warning restore IDE0060 // Remove unused parameter
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
+
+#pragma warning disable S125 // Sections of code should not be commented out
             //var eventData = req
             //return await @this.RunAsync(req, CancellationToken.None);
+#pragma warning restore S125 // Sections of code should not be commented out
             return new HttpResponseMessage();
         }
     }
