861ed543e9853ef789f39ac58062eb139dbc2904
Giulio Vian <giuliovdev@hotmail.com>
2022-03-26 15:38:10 +0000

    Refactor: move persistance code outside WorkItemStore

---
+++ src/aggregator-ruleng/Persistance/PersistByItem.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Work.WebApi.Contracts;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.WebApi;
+using Microsoft.VisualStudio.Services.WebApi.Patch.Json;
+using Newtonsoft.Json;
+
+namespace aggregator.Engine.Persistance
+{
+    internal class PersistByItem : PersisterBase
+    {
+        public PersistByItem(EngineContext context)
+            : base(context) { }
+
+        internal async Task<(int created, int updated)> SaveChanges_ByItem(bool commit, bool impersonate, bool bypassrules, CancellationToken cancellationToken)
+        {
+            int createdCounter = 0;
+            int updatedCounter = 0;
+
+            var (createdWorkItems, updatedWorkItems, deletedWorkItems, restoredWorkItems) = _context.Tracker.GetChangedWorkItems();
+            foreach (var item in createdWorkItems)
+            {
+                if (commit)
+                {
+                    _context.Logger.WriteInfo($"Creating a {item.WorkItemType} workitem in {item.TeamProject}");
+                    _ = await _clients.WitClient.CreateWorkItemAsync(
+                        item.Changes,
+                        _context.ProjectName,
+                        item.WorkItemType,
+                        bypassRules: impersonate || bypassrules,
+                        cancellationToken: cancellationToken
+                    );
+                }
+                else
+                {
+                    _context.Logger.WriteInfo($"Dry-run mode: should create a {item.WorkItemType} workitem in {item.TeamProject}");
+                }
+
+                createdCounter++;
+            }
+
+            if (commit)
+            {
+                await RestoreAndDelete(restoredWorkItems, deletedWorkItems, cancellationToken);
+            }
+            else if (deletedWorkItems.Any() || restoredWorkItems.Any())
+            {
+                static string FormatIds(WorkItemWrapper[] items) => string.Join(",", items.Select(item => item.Id));
+                var teamProjectName = restoredWorkItems.FirstOrDefault()?.TeamProject ??
+                                      deletedWorkItems.FirstOrDefault()?.TeamProject;
+                _context.Logger.WriteInfo($"Dry-run mode: should restore: {FormatIds(restoredWorkItems)} and delete {FormatIds(deletedWorkItems)} workitems from {teamProjectName}");
+            }
+            updatedCounter += restoredWorkItems.Length + deletedWorkItems.Length;
+
+            foreach (var item in updatedWorkItems.Concat(restoredWorkItems))
+            {
+                if (commit)
+                {
+                    _context.Logger.WriteInfo($"Updating workitem {item.Id}");
+                    _ = await _clients.WitClient.UpdateWorkItemAsync(
+                        item.Changes,
+                        item.Id,
+                        bypassRules: impersonate || bypassrules,
+                        cancellationToken: cancellationToken
+                    );
+                }
+                else
+                {
+                    _context.Logger.WriteInfo($"Dry-run mode: should update workitem {item.Id} in {item.TeamProject}");
+                }
+
+                updatedCounter++;
+            }
+
+            return (createdCounter, updatedCounter);
+        }
+    }
+}
---
+++ src/aggregator-ruleng/Persistance/PersisterBase.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Work.WebApi.Contracts;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.WebApi;
+using Microsoft.VisualStudio.Services.WebApi.Patch.Json;
+using Newtonsoft.Json;
+
+namespace aggregator.Engine.Persistance
+{
+    internal class PersisterBase
+    {
+        protected readonly EngineContext _context;
+        protected readonly IClientsContext _clients;
+
+        protected PersisterBase(EngineContext context)
+        {
+            _context = context;
+            _clients = _context.Clients;
+        }
+
+        protected async Task RestoreAndDelete(IEnumerable<WorkItemWrapper> restore, IEnumerable<WorkItemWrapper> delete, CancellationToken cancellationToken = default)
+        {
+            foreach (var item in delete)
+            {
+                _context.Logger.WriteInfo($"Deleting workitem {item.Id} in {item.TeamProject}");
+                _ = await _clients.WitClient.DeleteWorkItemAsync(item.Id, cancellationToken: cancellationToken);
+            }
+
+            foreach (var item in restore)
+            {
+                _context.Logger.WriteInfo($"Restoring workitem {item.Id} in {item.TeamProject}");
+                _ = await _clients.WitClient.RestoreWorkItemAsync(new WorkItemDeleteUpdate() { IsDeleted = false }, item.Id, cancellationToken: cancellationToken);
+            }
+        }
+
+        protected async Task<IEnumerable<WitBatchResponse>> ExecuteBatchRequest(IList<WitBatchRequest> batchRequests, CancellationToken cancellationToken)
+        {
+            if (!batchRequests.Any()) return Enumerable.Empty<WitBatchResponse>();
+
+            var batchResponses = await _clients.WitClient.ExecuteBatchRequest(batchRequests, cancellationToken: cancellationToken);
+
+            var failedResponses = batchResponses.Where(batchResponse => !IsSuccessStatusCode(batchResponse.Code)).ToList();
+            foreach (var failedResponse in failedResponses)
+            {
+                string stringResponse = JsonConvert.SerializeObject(batchResponses, Formatting.None);
+                _context.Logger.WriteVerbose(stringResponse);
+                _context.Logger.WriteError($"Save failed: {failedResponse.Body}");
+            }
+
+            //TODO should we throw exception?
+#pragma warning disable S125 // Sections of code should not be commented out
+            //if (failedResponses.Any())
+            //{
+            //    throw new InvalidOperationException("Save failed.");
+            //}
+#pragma warning restore S125 // Sections of code should not be commented out
+            return batchResponses;
+        }
+
+        private static bool IsSuccessStatusCode(int statusCode)
+        {
+            return (statusCode >= 200) && (statusCode <= 299);
+        }
+    }
+}
