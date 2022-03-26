965c2c1e6311c1162bd4f6366b73a6c5bb5f1ff2
Giulio Vian <giuliovdev@hotmail.com>
2022-03-26 14:10:46 +0000

    re-organize files in ruleng project

---
+++ src/aggregator-ruleng/Extensions/WorkItemExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Common;
+
+
+namespace aggregator.Engine
+{
+    public static class WorkItemExtension
+    {
+        public static string GetTeamProject(this WorkItem workItem)
+        {
+            return workItem.Fields.GetCastedValueOrDefault("System.TeamProject", default(string));
+        }
+    }
+}
